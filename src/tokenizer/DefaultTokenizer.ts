a27d2926e7cb9440e51ac4ad8526af0154c88ecc
naveen <kmnaveen101@gmail.com>
2022-02-20 09:02:31 +0530

    tokenizer test fails

--- src/tokenizer/DefaultTokenizer.ts
+++ src/tokenizer/DefaultTokenizer.ts
@@ -2,14 +2,38 @@ import {Token, Tokenizer} from "./Tokenizer";
 
 
 export class DefaultTokenizer implements Tokenizer {
-    constructor(code: string) {
-    }
+  private readonly characters: string[];
+  private lineNumber: number;
+  private characterPos: number;
+  private readonly tokens: Token[];
 
+  constructor(code: string) {
+    this.characters = code.split('');
+    this.lineNumber = 1;
+    this.characterPos = 1;
+    this.tokens = []
+  }
 
-    getTokens(): Token[] {
-      return [];
+  getTokens(): Token[] {
+    this.iterateThroughTokens();
+    return this.tokens;
+  }
+
+  private consume(): string {
+    return this.characters.shift();
+  }
+
+  private peek(): string {
+    // @TODO
+    return '';
+  }
+
+  private iterateThroughTokens() {
+
+    while (this.characters.length !== 0) {
+      const character = this.consume();
     }
 
   }
 
-
+}
